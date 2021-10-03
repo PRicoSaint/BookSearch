ee7270239de867494ebaba4ab2e9b5eea3c1883d
Esteban Santos <santosesteban09@gmail.com>
2021-10-02 22:07:50 -0500

    Added several updates to server side

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                }
+    }
+  }
+`;
+
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $title: String!, $bookId: String!, $description: String, $image: String, $link: String) {
+    saveBook(authors: $authors, title: $title, bookId: $bookId, description: $description, image: $image, link: $link) {
+      _id
+      authors
+      title
+      description
+      bookId
+      image
+      link
+    }
+  }
+`;
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      authors
+      title
+      description
+      bookId
+      image
+      link
+    }
+  }
+`;
